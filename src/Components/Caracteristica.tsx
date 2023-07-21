19d3f024815dacd0d5adc00fbc2fdb80d7a3972e
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-07-21 11:05:37 -0300

    Update Caracteristica.tsx

--- src/Components/Caracteristica.tsx
+++ src/Components/Caracteristica.tsx
@@ -13,14 +13,16 @@ export default function Caracteristica({
   body,
 }: CaracteristicaProps) {
   return (
-    <div className="border border-black rounded-lg m-4 p-8 flex flex-col justify-center items-center md:w-[33%] md:m-2 md:p-2">
+    <div
+      className={`border border-black rounded-lg m-4 p-8 flex flex-col justify-center items-center md:w-[33%] md:m-2 md:p-2`}
+    >
       <AnimatedIcon
         estatico={image.esta}
         dinamico={image.dina}
         alt={'image'}
         prop={60}
       />
-      <h1 className="text-center text-2xl text-orange-700 font-bold pt-4">
+      <h1 className={`text-center text-2xl text-orange-700 font-bold pt-4`}>
         {title}
       </h1>
       <p className="text-center text-sm">{body}</p>
